e1d73c854f287891d8d04dc215f13eb8ac7e93c3
hyfsy <1577975140@qq.com>
2020-06-20 14:10:47 +0800

    java agent asm外壳

---
+++ src/basics/agent/asm/Test.java
@@ -0,0 +1,13 @@
+package basics.agent.asm;
+
+/**
+ *
+ * 比较复杂的java类字节码操作类，需要对类加载和类字节码有较深的了解
+ *
+ * 参考：https://www.cnblogs.com/liuling/archive/2013/05/25/asm.html
+ *
+ * @author baB_hyf
+ * @date 2020/06/20
+ */
+public class Test {
+}
