f20cd0b83d147602a8a1642a28d7d36b79499852
hyfsy <1577975140@qq.com>
2021-04-18 18:43:38 +0800

    添加有意思的注释

---
+++ src/basics/text/Text.java
@@ -0,0 +1,14 @@
+package basics.text;
+
+/**
+ * @author baB_hyf
+ * @date 2021/04/15
+ */
+public class Text {
+
+    // <editor-fold defaultstate="collapsed" desc="Close Code">
+    public void text() {
+
+    }
+    // </editor-fold>
+}
